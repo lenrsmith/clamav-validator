fe648783df97b23df31e4c807bc95b306df0cc09
Martyn Ling <mling@orthomeo.com>
2015-07-29 22:02:23 +0100

    Changes required to make it work for AEA.

--- src/Lenrsmith/ClamavValidator/ClamavValidator.php
+++ src/Lenrsmith/ClamavValidator/ClamavValidator.php
@@ -37,7 +37,7 @@ class ClamavValidator extends Validator
 
 		// Set the permissions to allow us to scan file uploaded by PHP
 		// May need to set to 0666 on some systems.
-		chmod($file, '0660');
+		chmod($file, 0660);
 
 		// Create a new socket instance
 		$socket = (new \Socket\Raw\Factory())->createClient(self::CLAMAV_UNIX_SOCKET);
