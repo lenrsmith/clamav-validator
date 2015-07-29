186316a37bf2e14950246f9ffa451367f6b1f4df
Leonard Smith <lenrsmith@gmail.com>
2015-07-29 09:07:36 -0500

    Fixed permission on uploaded file to allow clam daemon to scan it

--- src/Lenrsmith/ClamavValidator/ClamavValidator.php
+++ src/Lenrsmith/ClamavValidator/ClamavValidator.php
@@ -35,6 +35,10 @@ class ClamavValidator extends Validator
 	{
 		$file = $this->getFilePath($value);
 
+		// Set the permissions to allow us to scan file uploaded by PHP
+		// May need to set to 0666 on some systems.
+		chmod($file, '0660');
+
 		// Create a new socket instance
 		$socket = (new \Socket\Raw\Factory())->createClient(self::CLAMAV_UNIX_SOCKET);
 		
@@ -75,4 +79,8 @@ class ClamavValidator extends Validator
 		// fallback: we were likely passed a path already
 		return $file;
 	}	
+
+	protected function replaceClamav($message, $attribute, $rule, $parameters){
+		return 'ClamAV detected a virus in '.$attribute.'.';
+	}
 }
